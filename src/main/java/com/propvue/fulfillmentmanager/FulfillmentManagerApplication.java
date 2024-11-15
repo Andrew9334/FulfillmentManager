c114d79e98c0a1f422fe9fbd8132e7fce3515035
Andrew <andfrewluferenko@icloud.com>
2024-11-15 10:13:26 +0300

    initial commit

---
+++ src/main/java/com/propvue/fulfillmentmanager/FulfillmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.propvue.fulfillmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillmentManagerApplication.class, args);
+	}
+
+}
